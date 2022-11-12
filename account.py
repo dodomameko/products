9a2c554687c4c4039b0520ff23646e3bf6261b34
leyun yue <dryadg953@gmail.com>
2022-11-12 15:30:07 +0800

    add account.py

---
+++ account.py
@@ -0,0 +1,11 @@
+#讓使用者重複的輸入商品名稱->迴圈 適合在不知道次數的情況下使用
+products = []
+while True:
+	name = input('請輸入商品名稱: ')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格: ')
+	p = [name,price]
+	products.append(p)
+	#products.append('name','price')
+print(products)
