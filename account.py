1a13136d6d8e911c93d57d731457bfc5790220ad
leyun yue <dryadg953@gmail.com>
2022-11-16 22:02:32 +0800

    ”read”

--- account.py
+++ account.py
@@ -1,14 +1,34 @@
 #讓使用者重複的輸入商品名稱->迴圈 適合在不知道次數的情況下使用
+
+#讀取檔案
+
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
+	price = int(price)
 	p = [name,price]
 	products.append(p)
 	#products.append('name','price')
 print(products)
 
+#將清單寫入到檔案中
 for p in products:
 	print(p[0],'的價格是',p[1])
+
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
+
+
